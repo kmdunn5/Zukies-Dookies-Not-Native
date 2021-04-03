81a845c15195dea46bafb2ab236fda46df3afbac
kmdunn5 <kmdunn5@gmail.com>
2021-04-03 18:56:36 -0400

    added a little to login component

---
+++ zukies-dookies/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+    constructor(props) 
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login
