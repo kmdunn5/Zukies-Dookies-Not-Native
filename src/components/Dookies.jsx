47cc72459798918de17b6373e911ac620e6c0092
kmdunn5 <kmdunn5@gmail.com>
2021-04-12 12:41:11 -0400

    fixed baseUrl to relate to backend

--- src/components/Dookies.jsx
+++ src/components/Dookies.jsx
@@ -91,7 +91,7 @@ let baseUrl
 if (process.env.NODE_ENV === 'development') {
     baseUrl = 'http://localhost:5000';
 } else {
-    baseUrl = 'https://zookies-dookies.herokuapp.com';
+    baseUrl = 'https://zookies-dookies-backend.herokuapp.com';
 };
 
 let api = '/api/v1/'
