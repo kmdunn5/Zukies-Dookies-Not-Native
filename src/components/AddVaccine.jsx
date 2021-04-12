0f969fe3d6924e426a7b957409a6ecea9642a693
kmdunn5 <kmdunn5@gmail.com>
2021-04-12 13:47:50 -0400

    updated all Url logic

--- src/components/AddVaccine.jsx
+++ src/components/AddVaccine.jsx
@@ -3,10 +3,10 @@ import Axios from 'axios'
 
 let baseUrl
 
-if (process.env.NODE_ENV === 'production') {
-    baseUrl = process.env.API_URL;
-} else {
+if (process.env.NODE_ENV === 'development') {
     baseUrl = 'http://localhost:5000';
+} else {
+    baseUrl = 'https://zookies-dookies-backend.herokuapp.com';
 };
 let api = '/api/v1/'
 
--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -24,10 +24,10 @@ const randomPhoto = () => {
 
 let baseUrl
 
-if (process.env.NODE_ENV === 'production') {
-    baseUrl = process.env.API_URL;
-} else {
+if (process.env.NODE_ENV === 'development') {
     baseUrl = 'http://localhost:5000';
+} else {
+    baseUrl = 'https://zookies-dookies-backend.herokuapp.com';
 };
 
 let api = '/api/v1/'
