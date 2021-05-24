d73e38c6c734dbe03aed8fae4d8d81a2d0dc3084
kmdunn5 <kmdunn5@gmail.com>
2021-05-24 12:26:41 -0400

    fixed how app.js gets the url

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ import DogShow from './components/dogs/DogShow'
 import { Switch, Route, Redirect } from 'react-router'
 import './App.css'
 
-let baseUrl
-
-if (process.env.NODE_ENV === 'development') {
-    baseUrl = 'http://localhost:5000';
-} else {
-    baseUrl = 'https://zookies-dookies-backend.herokuapp.com';
-};
-let api = '/api/v1/'
+import {baseUrl, api} from './baseUrl.js'
 
 class App extends Component{
   constructor(props) {
